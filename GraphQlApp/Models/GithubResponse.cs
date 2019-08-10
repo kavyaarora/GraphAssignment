bd49c3dff6c274094489569bbcb255eedf158650
kavya <kavyaarora.it09@gmail.com>
2019-08-09 23:59:05 -0400

    * .gitignore: Commit
    * GraphQlApp.sln:
    * App.xaml:
    * App.xaml.cs:
    * Main.cs:
    * MainPage.xaml:
    * Info.plist:
    * AssemblyInfo.cs:
    * MainPage.xaml.cs:
    * GraphQlApp.csproj:
    * AppDelegate.cs:
    * Entitlements.plist:
    * MainActivity.cs:
    * GithubResponse.cs:
    * GraphQlApp.iOS.csproj:
    * Default.png:
    * Default@2x.png:
    * AssemblyInfo.cs:
    * AboutAssets.txt:
    * GraphQlApp.Android.csproj:
    * Default-568h@2x.png:
    * AssemblyInfo.cs:
    * Default-Portrait.png:
    * Tabbar.xml:
    * colors.xml:
    * styles.xml:
    * AboutResources.txt:
    * Toolbar.xml:
    * LaunchScreen.storyboard:
    * Default-Portrait@2x.png:
    * Resource.designer.cs:
    * icon.png:
    * AndroidManifest.xml:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.xml:
    * Icon87.png:
    * Icon80.png:
    * Icon29.png:
    * Icon40.png:
    * Icon58.png:
    * Icon20.png:
    * Icon76.png:
    * Icon60.png:
    * icon_round.xml:
    * Icon180.png:
    * Icon152.png:
    * Icon120.png:
    * Icon167.png:
    * Icon1024.png:
    * Contents.json:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:

---
+++ GraphQlApp/Models/GithubResponse.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+
+namespace  GraphQlApp
+{
+    using System;
+using System.Collections.Generic;
+
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+public partial class GithubResponse
+{
+    [JsonProperty("data", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+    public Data Data { get; set; }
+}
+
+public partial class Data
+{
+    [JsonProperty("allFilms", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+    public AllFilms AllFilms { get; set; }
+}
+
+public partial class AllFilms
+{
+    [JsonProperty("films", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+    public Film[] Films { get; set; }
+}
+
+public partial class Film
+{
+    [JsonProperty("id", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+    public string Id { get; set; }
+
+    [JsonProperty("title", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+    public string Title { get; set; }
+
+    [JsonProperty("director", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+    public string Director { get; set; }
+
+
+}
+}
+
+
+
+
+
+
+
+
